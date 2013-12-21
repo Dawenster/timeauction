efb4b65f86076524c520dd8707448e51a8c3da01
David Wen <dstwen@gmail.com>
2013-12-21 11:47:25 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timeauction::Application.config.secret_key_base = '555bb19f69633abf257dc4274203bcb1883387163eaa525a14df2495da6671d3381bab9582c26c5da492c744e29c03bc6a88876b6d296b82f463f20402292301'
