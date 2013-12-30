0002cdfe30f7f2b334b602bf01269809339b79b5
David Wen <dstwen@gmail.com>
2013-12-30 12:31:05 -0800

    config.assets.compile set to true for Foundation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Timeauction::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true # changed for Zurb Foundation
 
   # Generate digests for assets URLs.
   config.assets.digest = true
