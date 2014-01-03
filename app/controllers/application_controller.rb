7f7ea03e62af708b1f2bc1c0fccdbb3fdb0df22a
David Wen <dstwen@gmail.com>
2014-01-03 11:24:27 -0800

    add username to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name
-    devise_parameter_sanitizer.for(:sign_up) << :last_name
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
     devise_parameter_sanitizer.for(:account_update) << :first_name
     devise_parameter_sanitizer.for(:account_update) << :last_name
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
   devise :omniauthable, :omniauth_providers => [:facebook]
 
-  validates :first_name, :last_name, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
 
   has_many :auctions
   has_and_belongs_to_many :tiers
 
-  def full_name
-    "#{self.first_name} #{self.last_name}"
+  before_save :create_username
+
+  def display_name
+    if self.first_name && self.last_name
+      "#{self.first_name} #{self.last_name}"
+    else
+      self.username
+    end
   end
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
@@ -19,6 +26,7 @@ class User < ActiveRecord::Base
       user = User.create(
         first_name: auth.extra.raw_info.first_name,
         last_name: auth.extra.raw_info.last_name,
+        username: auth.extra.raw_info.username,
         timezone: auth.extra.raw_info.timezone,
         gender: auth.extra.raw_info.gender,
         facebook_image: auth.info.image,
@@ -30,5 +38,13 @@ class User < ActiveRecord::Base
     end
     user.skip_confirmation!
     user
+  end
+
+  def create_username
+    self.username = self.to_slug
+  end
+
+  def to_slug
+    self.username.parameterize
   end
 end
