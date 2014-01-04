908331d98a5c5349cc87dbeaf9be8fbb788e9c28
David Wen <dstwen@gmail.com>
2014-01-03 20:16:59 -0800

    add bottom bar to auction grid

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -19,4 +19,16 @@ class Auction < ActiveRecord::Base
                     :styles => { :thumb => "300x225#", :display => "600x450#" },
                     :s3_credentials => s3_credentials_hash,
                     :bucket => "timeauction"
+
+  def hours_raised
+    self.tiers.inject(0) {|sum, tier| sum + tier.amount * tier.users.count }
+  end
+
+  def raised_percentage
+    "#{(hours_raised.to_f / self.target * 100).round}%"
+  end
+
+  def status
+    "<b>#{self.hours_raised}</b> hours raised, <b>#{self.raised_percentage}</b> of target".html_safe
+  end
 end
