95338dbf41186c2ffc6556dc915c0e01314ec1ae
David Wen <dstwen@gmail.com>
2014-01-06 15:36:44 -0800

    fix redirect loop on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,16 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    request.referrer
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    else
+      sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+      if request.referer == sign_in_url
+        super
+      else
+        stored_location_for(resource) || request.referer || root_path
+      end
+    end
   end
 
   protected
