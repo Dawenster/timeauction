10834f169e616b3ee3400019cdcbb354c40f6237
David Wen <dstwen@gmail.com>
2014-01-07 11:33:17 -0800

    add meta tags to auctions#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,12 @@ module ApplicationHelper
     return nil if date.nil?
     date.strftime("%b %d, %Y")
   end
+
+  def generate_meta_tags(title, description, image)
+    meta :title => title, :description => description
+    meta [:property => "og:image", :content => image] unless image.blank?
+    meta [:property => "og:title", :content => title]
+    meta [:property => "og:url", :content => request.original_url]
+    meta [:property => "og:description", :content => description]
+  end
 end
