369eb149ef2e17eb60bf5ecacdf83cdbdf9c8b8a
David Wen <dstwen@gmail.com>
2014-01-06 16:19:58 -0800

    attempted fix of redirect loop on login/signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,7 @@ class ApplicationController < ActionController::Base
     if request.env['omniauth.origin']
       request.env['omniauth.origin'] || stored_location_for(resource) || root_path
     else
-      sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-      if request.referer == sign_in_url
+      if referer_match?
         super
       else
         stored_location_for(resource) || request.referer || root_path
@@ -28,5 +27,17 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) do |u|
       u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
     end
+  end
+
+  def referer_match?
+    sign_in_url = new_session_url(resource)
+    sign_up_url = new_registration_url(resource)
+    if Rails.env.production?
+      failed_sign_in_url = "http://" + request.host + "/users"
+    else
+      failed_sign_in_url = "http://" + request.host + ":" + request.port.to_s + "/users"
+    end
+
+    return request.referer == sign_in_url || request.referer == sign_up_url || request.referer == failed_sign_in_url
   end
 end
