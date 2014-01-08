71fc080920f596a964b724a803e8995b27130109
David Wen <dstwen@gmail.com>
2014-01-07 22:52:10 -0800

    show only submitted auctions on the landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def landing
-    @recent_auctions = Auction.order("created_at DESC").limit(3)
-    @featured_auctions = (Auction.all - @recent_auctions).sample(3)
+    @recent_auctions = Auction.where(:submitted => true).order("created_at DESC").limit(3)
+    @featured_auctions = (Auction.where(:submitted => true) - @recent_auctions).sample(3)
   end
 end
