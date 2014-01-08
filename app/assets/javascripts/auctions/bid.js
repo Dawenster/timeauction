dae1c26c1f29e83880cf8062085319be7d7d01af
David Wen <dstwen@gmail.com>
2014-01-07 22:39:22 -0800

    add validation to make sure users enter first and last name before committing hours

--- app/assets/javascripts/auctions/bid.js
+++ app/assets/javascripts/auctions/bid.js
@@ -1,24 +1,37 @@
 $(document).ready(function() {
   $("body").on("click", ".commit-button", function(e) {
     e.preventDefault();
-    var firstName = null;
-    var lastName = null;
 
-    if ($(".name-field").length > 0) {
-      firstName = $(".first-name").val();
-      lastName = $(".last-name").val();
+    if ($(".first-name").val() != "" && $(".last-name").val() != "") {
+      var firstName = null;
+      var lastName = null;
+
+      if ($(".name-field").length > 0) {
+        firstName = $(".first-name").val();
+        lastName = $(".last-name").val();
+      }
+
+      $.ajax({
+        url: $(this).parent().attr("action"),
+        method: "put",
+        data: {
+          first_name: firstName,
+          last_name: lastName
+        }
+      })
+      .done(function(data) {
+        window.location = data.url;
+      });
+    } else {
+      $(".error").remove();
+      var nameFields = $(".name-field");
+      for (var i = 0; i < nameFields.length; i++) {
+        if ($(nameFields[i]).val() == "") {
+          $(nameFields[i]).after("<small class='error'>Please fill in</small>");
+        }
+      }
     }
 
-    $.ajax({
-      url: $(this).parent().attr("action"),
-      method: "put",
-      data: {
-        first_name: firstName,
-        last_name: lastName
-      }
-    })
-    .done(function(data) {
-      window.location = data.url;
-    });
   });
+
 });
