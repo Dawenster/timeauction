12f4f243d18a586ff01dad792eed956541525157
David Wen <dstwen@gmail.com>
2014-01-07 17:06:51 -0800

    updated landing page with auctions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def landing
-    
+    @recent_auctions = Auction.order("created_at DESC").limit(3)
+    @featured_auctions = (Auction.all - @recent_auctions).sample(3)
   end
 
   def how_it_works
