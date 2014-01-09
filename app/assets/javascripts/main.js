b9469aa3ece311740a82ee014dcdc436287b513a
David Wen <dstwen@gmail.com>
2014-01-08 16:36:30 -0800

    added explanation modal for the fact that we're currently a demo site only

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+  var shownDemo = localStorage.getItem('demo-explanation');
+  if (!shownDemo) {
+    $('#demo-explanation').foundation('reveal', 'open', '');
+  }
+
   $("body").on("click", ".close-reveal-modal", function(e) {
     e.preventDefault();
     $(".reveal-modal").foundation('reveal', 'close', '');
@@ -7,5 +12,12 @@ $(document).ready(function() {
   $("body").on("click", ".reveal-modal-bg", function(e) {
     e.preventDefault();
     $(".reveal-modal").foundation('reveal', 'close', '');
+    localStorage.setItem('demo-explanation', JSON.stringify(true));
+  });
+
+  $("body").on("click", ".demo-close", function(e) {
+    e.preventDefault();
+    $(".reveal-modal").foundation('reveal', 'close', '');
+    localStorage.setItem('demo-explanation', JSON.stringify(true));
   });
 });
