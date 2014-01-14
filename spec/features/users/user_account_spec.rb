0883c9b24f3f4434bb4ed57d2ab5b73bb25020fe
David Wen <dstwen@gmail.com>
2014-01-13 17:20:03 -0800

    can change username and email

--- spec/features/users/user_account_spec.rb
+++ spec/features/users/user_account_spec.rb
@@ -4,12 +4,30 @@ describe "User account" do
   subject { page }
   let!(:user) { FactoryGirl.create :user, :email => "johndoe@email.com" }
 
-  it "can change account details" do
+  before do
     facebook_login
     visit edit_user_registration_path
+  end
+
+  it "can change first and last name" do
     fill_in :user_first_name, :with => "Sexy New"
     fill_in :user_last_name, :with => "Name"
     click_on "Update"
     page.should have_content("Sexy New Name")
+  end
+
+  it "can change username" do
+    within(".main-section") do
+      fill_in :user_username, :with => "sExY vOlUnTeEr"
+      fill_in :user_email, :with => "sexy@newemail.com"
+    end
+    click_on "Update"
+    visit edit_user_registration_path
+    within(".main-section") do
+      find_field(:user_username)
+    end.value.should eq("sexy-volunteer")
+    within(".main-section") do
+      find_field(:user_email)
+    end.value.should eq("sexy@newemail.com")
   end
 end
