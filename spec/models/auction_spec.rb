d6f5d9ad77b21b724132fd5aa0ec02a56f19233b
David Wen <dstwen@gmail.com>
2014-01-13 21:47:12 -0800

    auctions presence validations

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,6 +1,21 @@
+require 'spec_helper'
+
+describe Auction do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:short_description) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:about) }
+  it { should validate_presence_of(:target) }
+  it { should validate_presence_of(:start) }
+  it { should validate_presence_of(:end) }
+  it { should validate_presence_of(:volunteer_end_date) }
+  it { should validate_presence_of(:banner) }
+  it { should validate_presence_of(:image) }
+
   context "when registering" do
     let(:auction) { FactoryGirl.create :auction }
     let(:user) { FactoryGirl.create :user, :email => "johndoe@email.com" }
+
     it "#hours_raised" do
       auction.rewards.each do |reward|
         reward.users << user
