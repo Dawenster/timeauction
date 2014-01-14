ec6c8072987d145ac6db28310047e73601886c4e
David Wen <dstwen@gmail.com>
2014-01-14 13:59:47 -0800

    testing remainder of auctions.rb methods

--- spec/factories/auctions.rb
+++ spec/factories/auctions.rb
@@ -25,6 +25,3 @@ FactoryGirl.define do
   end
 
 end
-
-# approved
-# submitted
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -29,5 +29,54 @@ describe Auction do
       auction.update_attributes(:target => 10)
       expect(auction.raised_percentage).to eq("300%")
     end
+
+    it "#rewards_ordered_by_lowest" do
+      expect(auction.rewards_ordered_by_lowest.first).to eq(auction.rewards.first)
+    end
+
+    it "#lowest_bid" do
+      expect(auction.lowest_bid).to eq(10)
+    end
+
+    it "#volunteers" do
+      expect(auction.volunteers).to eq([user])
+    end
+
+    it "#num_volunteers" do
+      expect(auction.num_volunteers).to eq(1)
+    end
+
+    it "#days_left_to_bid > 48 hours" do
+      expect(auction.days_left_to_bid).to eq([6, "days"])
+    end
+
+    it "#days_left_to_bid < 48 hours" do
+      auction.update_attributes(:end => Time.now + 1.day)
+      expect(auction.days_left_to_bid).to eq([23, "hours"])
+    end
+
+    it "#average_bid" do
+      expect(auction.average_bid).to eq("30")
+    end
+
+    it "is #pending_approval" do
+      auction.update_attributes(:submitted => true)
+      expect(auction.pending_approval).to eq(true)
+    end
+
+    it "is not #pending_approval" do
+      expect(auction.pending_approval).to eq(false)
+      auction.update_attributes(:submitted => true, :approved => true)
+      expect(auction.pending_approval).to eq(false)
+    end
+
+    it "is #over?" do
+      auction.update_attributes(:end => Time.now - 1.day)
+      expect(auction.over?).to eq(true)
+    end
+
+    it "is not #over?" do
+      expect(auction.over?).to eq(false)
+    end
   end
 end
