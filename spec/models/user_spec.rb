53baa13288eb2e0d1dcb3ce1e6281a68df3e049c
David Wen <dstwen@gmail.com>
2014-01-13 16:29:48 -0800

    validate presence of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe User do
 
+  it { should validate_presence_of(:email) }
   it { should validate_presence_of(:username) }
   it { should validate_uniqueness_of(:username) }
 
