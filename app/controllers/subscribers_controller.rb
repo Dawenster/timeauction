6037640d9062d2a1cc7b68683709fe8b4059eb66
David Wen <dstwen@gmail.com>
2014-01-20 17:00:09 +0800

    check for legitimate email in subscribers controller

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,16 +1,21 @@
 class SubscribersController < ApplicationController
   def create
     respond_to do |format|
-      existing_subscriber = Subscriber.find_by_email(params[:subscriber][:email])
+      email = params[:subscriber][:email].downcase
+      existing_subscriber = Subscriber.find_by_email(email)
       if existing_subscriber
         existing_subscriber.update_attributes(:user_id => params[:subscriber][:user_id]) if params[:subscriber][:user_id]
         format.json { render :json => {:message => "#{existing_subscriber.email} is already subscribed :)"} }
       else
         @subscriber = Subscriber.new(subscriber_params)
-        if @subscriber.save
-          format.json { render :json => {:message => "#{@subscriber.email} has been added successfully."} }
+        if legit_email?(email)
+          if @subscriber.save
+            format.json { render :json => {:message => "#{@subscriber.email} has been added successfully."} }
+          else
+            format.json { render :json => {:message => "Hm... something went wrong... please try again later!"} }
+          end
         else
-          format.json { render :json => {:message => "Hm... something went wrong... please try again later!"} }
+          format.json { render :json => {:message => "Please enter a legitimate email!"} }
         end
       end
     end
@@ -23,5 +28,9 @@ class SubscribersController < ApplicationController
       :email,
       :user_id
     )
+  end
+
+  def legit_email?(string)
+    !!(string =~ /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
   end
 end
