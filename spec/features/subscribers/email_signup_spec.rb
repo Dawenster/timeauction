4c76ab8d8354ed828f67480aebc9d14b6760ca26
David Wen <dstwen@gmail.com>
2014-01-23 21:36:44 +0800

    specs for email sign up on auction page

--- spec/features/subscribers/email_signup_spec.rb
+++ spec/features/subscribers/email_signup_spec.rb
@@ -10,16 +10,53 @@ describe "email signup" do
     let!(:bid_2) { FactoryGirl.create :bid, :reward_id => auction.rewards.last.id, :user_id => user.id }
 
     before do
+      auction.update_attributes(:target => 10)
       visit auction_path(auction)
     end
 
     context "signed in" do
-      let!(:creator) { FactoryGirl.create :user }
 
+      before do
+        facebook_login
+      end
+
+      it "subscriber input defaults value with user email", :js => true do
+        find_field("subscriber_email").value.should eq(user.email)
+      end
+
+      it "can sign up", :js => true do
+        expect do
+          click_on "Subscribe"
+        end.to change(Subscriber, :count).by(1)
+        page.should have_content("has been added successfully")
+      end
     end
 
     context "not signed in" do
+      it "can sign up with legit email", :js => true do
+        fill_in :subscriber_email, :with => "legitemail@gmail.com"
+        expect do
+          click_on "Subscribe"
+        end.to change(Subscriber, :count).by(1)
+        page.should have_content("has been added successfully")
+      end
 
+      it "cannot sign up with non-legit email", :js => true do
+        fill_in :subscriber_email, :with => "notanemail"
+        expect do
+          click_on "Subscribe"
+        end.to change(Subscriber, :count).by(0)
+        page.should have_content("Please enter a legitimate email!")
+      end
+
+      it "recognizes existing emails", :js => true do
+        Subscriber.create(:email => "legitemail@gmail.com")
+        fill_in :subscriber_email, :with => "legitemail@gmail.com"
+        expect do
+          click_on "Subscribe"
+        end.to change(Subscriber, :count).by(0)
+        page.should have_content("is already subscribed")
+      end
     end
   end
 
