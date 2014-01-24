8349a6e1dcf4672937eb06fdec51b8344a8a9e76
David Wen <dstwen@gmail.com>
2014-01-24 22:24:31 +0800

    user auctions spec

---
+++ spec/features/users/user_auctions_spec.rb
@@ -0,0 +1,89 @@
+require 'spec_helper'
+
+describe "user auctions" do
+  subject { page }
+
+  let!(:user) { FactoryGirl.create :user, :email => "johndoe@email.com" }
+  let!(:auction) { FactoryGirl.create :auction_with_rewards, :rewards_count => 2, :user => user }
+  let!(:bid_1) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
+
+  before do
+    facebook_login
+  end
+
+  context "bid on" do
+    it "can see auctions bid on", :js => true do
+      visit user_auctions_path(user.username)
+      page.should have_content(auction.title, visible: true)
+    end
+
+    it "cannot see auctions bid on in other tabs", :js => true do
+      auction.update_attributes(:user_id => 200)
+      visit user_auctions_path(user.username)
+      click_on "Saved"
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Submitted"
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Approved"
+      page.should_not have_content(auction.title, visible: true)
+    end
+  end
+
+  context "saved" do
+    it "can see saved auctions", :js => true do
+      visit user_auctions_path(user.username)
+      click_on "Saved"
+      page.should have_content(auction.title, visible: true)
+    end
+
+    it "cannot see saved auctions in other tabs", :js => true do
+      bid_1.update_attributes(:user_id => 200)
+      visit user_auctions_path(user.username)
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Submitted"
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Approved"
+      page.should_not have_content(auction.title, visible: true)
+    end
+  end
+
+  context "submitted" do
+    it "can see submitted auctions", :js => true do
+      auction.update_attributes(:submitted => true)
+      visit user_auctions_path(user.username)
+      click_on "Submitted"
+      page.should have_content(auction.title, visible: true)
+    end
+
+    it "cannot see submitted auctions in other tabs", :js => true do
+      bid_1.update_attributes(:user_id => 200)
+      auction.update_attributes(:submitted => true)
+      visit user_auctions_path(user.username)
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Saved"
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Approved"
+      page.should_not have_content(auction.title, visible: true)
+    end
+  end
+
+  context "approved" do
+    it "can see approved auctions", :js => true do
+      auction.update_attributes(:submitted => true, :approved => true)
+      visit user_auctions_path(user.username)
+      click_on "Approved"
+      page.should have_content(auction.title, visible: true)
+    end
+
+    it "cannot see approved auctions in other tabs", :js => true do
+      bid_1.update_attributes(:user_id => 200)
+      auction.update_attributes(:submitted => true, :approved => true)
+      visit user_auctions_path(user.username)
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Saved"
+      page.should_not have_content(auction.title, visible: true)
+      click_on "Submitted"
+      page.should_not have_content(auction.title, visible: true)
+    end
+  end
+end
