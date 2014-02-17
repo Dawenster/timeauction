9283bddc39c47a4b3444de37d8cb340358b9a24a
David Wen <dstwen@gmail.com>
2014-02-16 17:43:45 -0800

    add custom confirmation email

---
+++ app/mailers/registration_mailer.rb
@@ -0,0 +1,12 @@
+class RegistrationMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+
+  default from: '"Time Auction Team" <team@timeauction.org>'
+
+  def confirmation_instructions(resource, token, opts={})
+    opts[:subject] = "Confirm your Time Auction account"
+    @username = resource.username
+    super
+  end
+end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,10 +10,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'confirmation@timeauction.org'
+  config.mailer_sender = '"Time Auction Team" <team@timeauction.org>'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'RegistrationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
