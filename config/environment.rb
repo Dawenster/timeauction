5fe70f7d84ba50fe6f12e444b4b97ac63982b3f3
David Wen <dstwen@gmail.com>
2014-02-16 17:43:27 -0800

    setup production email feature config

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load heroku vars from local file
+email_password_file = File.join(Rails.root, 'config', 'email_password.rb')
+load(email_password_file) if File.exists?(email_password_file)
+
 # Initialize the Rails application.
 Timeauction::Application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,16 @@ Timeauction::Application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  # Gmail Settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'timeauction.org',
+    user_name:            'team@timeauction.org',
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
