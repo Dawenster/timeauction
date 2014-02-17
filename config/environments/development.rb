9e1358bf7da05a33b981b7cb41ef855a94c4e993
David Wen <dstwen@gmail.com>
2014-02-16 17:42:44 -0800

    config for development environment for email features

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Timeauction::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -29,6 +29,8 @@ Timeauction::Application.configure do
 
   # Devise setting
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Mail catcher settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
@@ -42,4 +44,16 @@ Timeauction::Application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  # Gmail Settings
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'timeauction.org',
+  #   user_name:            'team@timeauction.org',
+  #   password:             ENV['EMAIL_PASSWORD'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true
+  # }
 end
