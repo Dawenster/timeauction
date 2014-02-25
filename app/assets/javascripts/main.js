98f980a9bdffa5dd45d7de60e42cbba012cdb8e6
David Wen <dstwen@gmail.com>
2014-02-24 21:42:46 -0800

    comment out demo modal

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
   var shownDemo = localStorage.getItem('demo-explanation');
-  if (!$.cookie('demo-explanation')) {
-    $('#demo-explanation').foundation('reveal', 'open', '');
-  }
+  // if (!$.cookie('demo-explanation')) {
+  //   $('#demo-explanation').foundation('reveal', 'open', '');
+  // }
 
-  $("body").on("click", ".close-reveal-modal", function(e) {
-    e.preventDefault();
-    $(".reveal-modal").foundation('reveal', 'close', '');
-  });
+  // $("body").on("click", ".close-reveal-modal", function(e) {
+  //   e.preventDefault();
+  //   $(".reveal-modal").foundation('reveal', 'close', '');
+  // });
 
-  $("body").on("click", ".reveal-modal-bg", function(e) {
-    e.preventDefault();
-    $(".reveal-modal").foundation('reveal', 'close', '');
-    $.cookie('demo-explanation', true);
-  });
+  // $("body").on("click", ".reveal-modal-bg", function(e) {
+  //   e.preventDefault();
+  //   $(".reveal-modal").foundation('reveal', 'close', '');
+  //   $.cookie('demo-explanation', true);
+  // });
 
-  $("body").on("click", ".demo-close", function(e) {
-    e.preventDefault();
-    $(".reveal-modal").foundation('reveal', 'close', '');
-    $.cookie('demo-explanation', true);
-  });
+  // $("body").on("click", ".demo-close", function(e) {
+  //   e.preventDefault();
+  //   $(".reveal-modal").foundation('reveal', 'close', '');
+  //   $.cookie('demo-explanation', true);
+  // });
 });
--- spec/features/demo_modal/demo_modal_spec.rb
+++ spec/features/demo_modal/demo_modal_spec.rb
@@ -1,41 +1,41 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe "demo modal" do
-  subject { page }
+# describe "demo modal" do
+#   subject { page }
 
-  it "displays demo modal", :js => true do
-    visit root_path
-    page.should have_content("Welcome to Time Auction", visible: true)
-  end
+#   it "displays demo modal", :js => true do
+#     visit root_path
+#     page.should have_content("Welcome to Time Auction", visible: true)
+#   end
 
-  context "after clicking close" do
-    it "does not display demo modal", :js => true do
-      visit root_path
-      within "#demo-explanation" do
-        find(".close-reveal-modal").click
-      end
-      visit terms_and_conditions_path
-      page.should_not have_content("Welcome to Time Auction", visible: true)
-    end
-  end
+#   context "after clicking close" do
+#     it "does not display demo modal", :js => true do
+#       visit root_path
+#       within "#demo-explanation" do
+#         find(".close-reveal-modal").click
+#       end
+#       visit terms_and_conditions_path
+#       page.should_not have_content("Welcome to Time Auction", visible: true)
+#     end
+#   end
 
-  context "after clicking outside the box" do
-    it "does not display demo modal", :js => true do
-      visit root_path
-      find(".reveal-modal-bg").click
-      visit terms_and_conditions_path
-      page.should_not have_content("Welcome to Time Auction", visible: true)
-    end
-  end
+#   context "after clicking outside the box" do
+#     it "does not display demo modal", :js => true do
+#       visit root_path
+#       find(".reveal-modal-bg").click
+#       visit terms_and_conditions_path
+#       page.should_not have_content("Welcome to Time Auction", visible: true)
+#     end
+#   end
 
-  context "after clicking let's see the site" do
-    it "does not display demo modal", :js => true do
-      visit root_path
-      within "#demo-explanation" do
-        find(".button").click
-      end
-      visit terms_and_conditions_path
-      page.should_not have_content("Welcome to Time Auction", visible: true)
-    end
-  end
-end
+#   context "after clicking let's see the site" do
+#     it "does not display demo modal", :js => true do
+#       visit root_path
+#       within "#demo-explanation" do
+#         find(".button").click
+#       end
+#       visit terms_and_conditions_path
+#       page.should_not have_content("Welcome to Time Auction", visible: true)
+#     end
+#   end
+# end
