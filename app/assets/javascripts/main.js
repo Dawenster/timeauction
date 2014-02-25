9bddb3b215748f5f3ae3646977290c10144dc709
David Wen <dstwen@gmail.com>
2014-02-25 14:54:36 -0800

    add back reveal close js functions in main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
   //   $('#demo-explanation').foundation('reveal', 'open', '');
   // }
 
-  // $("body").on("click", ".close-reveal-modal", function(e) {
-  //   e.preventDefault();
-  //   $(".reveal-modal").foundation('reveal', 'close', '');
-  // });
+  $("body").on("click", ".close-reveal-modal", function(e) {
+    e.preventDefault();
+    $(".reveal-modal").foundation('reveal', 'close', '');
+  });
 
-  // $("body").on("click", ".reveal-modal-bg", function(e) {
-  //   e.preventDefault();
-  //   $(".reveal-modal").foundation('reveal', 'close', '');
-  //   $.cookie('demo-explanation', true);
-  // });
+  $("body").on("click", ".reveal-modal-bg", function(e) {
+    e.preventDefault();
+    $(".reveal-modal").foundation('reveal', 'close', '');
+    // $.cookie('demo-explanation', true);
+  });
 
   // $("body").on("click", ".demo-close", function(e) {
   //   e.preventDefault();
