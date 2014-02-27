4629b57ca707c850e250f1452052f3a71fb2e5ef
David Wen <dstwen@gmail.com>
2014-02-26 22:20:18 -0800

    specs for upgrading account from edit account page

--- spec/features/bids/premium_spec.rb
+++ spec/features/bids/premium_spec.rb
@@ -10,75 +10,111 @@ describe "premium bids" do
   before do
     auction.update_attributes(:target => 10)
     auction.rewards.first.update_attributes(:premium => true)
-    visit auction_path(auction)
     facebook_login
     sleep 1
   end
 
-  context "when user has not upgraded", :js => true do
+  context "auctions#show" do
     before do
-      all(".bid-button").first.click
+      visit auction_path(auction)
     end
 
-    context "when reward is premium" do
-      it "opens upgrade modal" do
-        page.should have_selector('#upgrade-account-modal', visible: true)
+    context "when user has not upgraded", :js => true do
+      before do
+        all(".bid-button").first.click
       end
 
-      context "within upgrade modal" do
-        it "can click 'Upgrade'" do
-          find(".upgrade-button").click
-          page.should have_selector('#upgrade-payment-modal', visible: true)
-        end
-
-        it "can close modal by clicking text" do
-          find(".no-thanks-on-premium").click
-          page.should_not have_selector('#upgrade-account-modal', visible: true)
-        end
-      end
-
-      context "within upgrade payment modal" do
-        before do
-          find(".upgrade-button").click
-        end
-
-        it "can close modal by clicking text" do
-          sleep 1
-          find(".no-thanks-on-premium").click
-          sleep 1
-          page.should_not have_selector('#upgrade-payment-modal', visible: true)
-        end
-
-        it "can go back a modal" do
-          find(".back-on-upgrade-payment").click
+      context "when reward is premium" do
+        it "opens upgrade modal" do
           page.should have_selector('#upgrade-account-modal', visible: true)
         end
 
-        it "can switch who to donate to" do
-          find("#american-red-cross").click
-          page.should have_content('https://www.redcross.org/quickdonate/index.jsp')
+        context "within upgrade modal" do
+          it "can click 'Upgrade'" do
+            find(".upgrade-button").click
+            page.should have_selector('#upgrade-payment-modal', visible: true)
+          end
+
+          it "can close modal by clicking text" do
+            find(".no-thanks-on-premium").click
+            page.should_not have_selector('#upgrade-account-modal', visible: true)
+          end
         end
 
-        it "can switch the link of who to donate to" do
-          find(".upgrade-payment-button")[:href].should eq("http://www.redcross.ca/donate")
-          find("#american-red-cross").click
-          find(".upgrade-payment-button")[:href].should eq("https://www.redcross.org/quickdonate/index.jsp")
-        end
+        context "within upgrade payment modal" do
+          before do
+            find(".upgrade-button").click
+          end
 
-        it "upgrades the user after clicking 'Donate'" do
-          find(".upgrade-payment-button").click
-          sleep 1
-          User.last.premium.should eq(true)
+          it "can close modal by clicking text" do
+            sleep 1
+            find(".no-thanks-on-premium").click
+            sleep 1
+            page.should_not have_selector('#upgrade-payment-modal', visible: true)
+          end
+
+          it "can go back a modal" do
+            find(".back-on-upgrade-payment").click
+            page.should have_selector('#upgrade-account-modal', visible: true)
+          end
+
+          it "can switch who to donate to" do
+            find("#american-red-cross").click
+            page.should have_content('https://www.redcross.org/quickdonate/index.jsp')
+          end
+
+          it "can switch the link of who to donate to" do
+            find(".upgrade-payment-button")[:href].should eq("http://www.redcross.ca/donate")
+            find("#american-red-cross").click
+            find(".upgrade-payment-button")[:href].should eq("https://www.redcross.org/quickdonate/index.jsp")
+          end
+
+          it "upgrades the user after clicking 'Donate'" do
+            find(".upgrade-payment-button").click
+            sleep 1
+            User.last.premium.should eq(true)
+          end
         end
+      end
+    end
+
+    context "when user has upgraded", :js => true do
+      it "opens bid modal" do
+        user.update_attributes(:premium => true)
+        all(".bid-button").first.click
+        page.should have_selector('#bid-modal', visible: true)
       end
     end
   end
 
-  context "when user has upgraded", :js => true do
-    it "opens bid modal" do
-      user.update_attributes(:premium => true)
-      all(".bid-button").first.click
-      page.should have_selector('#bid-modal', visible: true)
+  context "user account page", :js => true do
+
+    context "user upgraded" do
+      it "shows the heart icon" do
+        user.update_attributes(:premium => true)
+        visit edit_user_registration_path
+        page.should have_css(".fa-heart")
+      end
+    end
+
+    context "user not upgraded" do
+      before do
+        visit edit_user_registration_path
+      end
+
+      it "shows the upgrade button" do
+        page.should have_css(".non-auction-page-upgrade")
+      end
+
+      it "shows the upgrade modal when upgrade button clicked" do
+        find(".non-auction-page-upgrade").click
+        page.should have_css("#upgrade-account-modal")
+      end
+
+      it "shows different text than the default" do
+        find(".non-auction-page-upgrade").click
+        page.should have_content("Upgrading your account")
+      end
     end
   end
 end
