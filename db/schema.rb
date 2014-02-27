4ce03765758fbaa3a686901a65120774bd1d865d
David Wen <dstwen@gmail.com>
2014-02-27 09:15:43 -0800

    migration: add_upgrade_date_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140226180231) do
+ActiveRecord::Schema.define(version: 20140227171442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20140226180231) do
     t.string   "facebook_image"
     t.string   "username"
     t.boolean  "premium",                default: false
+    t.datetime "upgrade_date"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
