6c4e9ddfcf16a919ab1bf3b39c7023e2e6e53523
David Wen <dstwen@gmail.com>
2014-02-28 14:55:06 -0800

    spec for num_on_waitlist

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -10,7 +10,6 @@ describe Reward do
     set(:auction) { FactoryGirl.create :auction_with_rewards, :rewards_count => 2 }
     set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com" }
     set(:bid_1) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
-    set(:bid_2) { FactoryGirl.create :bid, :reward_id => auction.rewards.last.id, :user_id => user.id }
 
     it "#num_bidders" do
       expect(auction.rewards.first.num_bidders).to eq(1)
@@ -33,6 +32,16 @@ describe Reward do
         expect(auction.rewards.first.maxed_out?).to eq(false)
       end
     end
-  end
 
+    context "waitlist bid" do
+      set(:bid_2) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
+
+      it "#num_on_waitlist" do
+        reward = auction.rewards.first
+        reward.update_attributes(:max => 1)
+
+        expect(reward.num_on_waitlist).to eq(1)
+      end
+    end
+  end
 end
