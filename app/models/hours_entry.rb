1443f220e6e43d6c61c916018a59cde7d760213a
David Wen <dstwen@gmail.com>
2014-03-14 14:03:37 -0700

    fix bug in hours_entry.rb validation; add email validation

--- app/models/hours_entry.rb
+++ app/models/hours_entry.rb
@@ -1,5 +1,6 @@
 class HoursEntry < ActiveRecord::Base
-  validates :amount, :organization, :contact_name, :contact_phone, :contact_email
+  validates :amount, :organization, :contact_name, :contact_phone, :contact_email, presence: true
+  validates :contact_email, :format => { :with => /\A[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})\z/, :message => "is not a valid email" }
 
   belongs_to :user
 end
