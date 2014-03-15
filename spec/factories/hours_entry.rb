5d03fe38509bea290b47670e04161e2858a4acbe
David Wen <dstwen@gmail.com>
2014-03-14 23:13:26 -0700

    update spec factories for hours_entry

---
+++ spec/factories/hours_entry.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :hours_entry do
+    amount 10
+    organization "Red Cross"
+    contact_name "Supervisor Dude"
+    contact_phone "123-456-7890"
+    contact_email "supervisor@dude.com"
+    contact_position "Supervisor"
+  end
+end
