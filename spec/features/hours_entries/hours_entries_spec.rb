2a2933490a5e078a162f58abcfbdadcb20ff133d
David Wen <dstwen@gmail.com>
2014-03-14 19:18:07 -0700

    specs for hours_entries #create

---
+++ spec/features/hours_entries/hours_entries_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe "hours entries" do
+  subject { page }
+
+  set(:creator) { FactoryGirl.create :user }
+  set(:auction) { FactoryGirl.create :auction_with_rewards, :rewards_count => 1, :user => creator }
+  set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com" }
+  set(:bid_1) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => 99 }
+
+  before do
+    login(user)
+  end
+
+  context "cannot submit hours" do
+    it "shows browse and upgrade buttons", :js => true do
+      visit hours_entries_path
+      page.should have_content("Browse auctions", visible: true)
+      page.should have_content("Upgrade account", visible: true)
+    end
+  end
+
+  context "can submit hours" do
+    context "as premium user" do
+      before do
+        user.update_attributes(:premium => true, :upgrade_date => Time.now)
+      end
+
+      it "shows submit new hours button", :js => true do
+        visit hours_entries_path
+        page.should have_content("Submit new hours", visible: true)
+      end
+
+      context "#create" do
+        before do
+          visit new_hours_entry_path
+        end
+
+        it "is successful" do
+          fill_in_hours_entries_form(10)
+          expect do
+            click_button "Submit for approval*"
+          end.to change(HoursEntry, :count).by(1)
+        end
+
+        it "shows email validation error" do
+          fill_in_hours_entries_form(10)
+          fill_in :hours_entry_contact_email, :with => "supervisor@"
+          expect do
+            click_button "Submit for approval*"
+          end.to change(HoursEntry, :count).by(0)
+          page.should have_content("not a valid email")
+        end
+
+        it "shows earned hours on #index", :js => true do
+          fill_in_hours_entries_form(10)
+          click_button "Submit for approval*"
+          visit hours_entries_path
+          page.should have_content("for Red Cross", visible: true)
+        end
+      end
+    end
+
+    context "as non-premium user with bids" do
+
+      before do
+        bid_1.update_attributes(:user_id => user.id)
+        visit hours_entries_path
+      end
+
+      it "shows submit new hours button" do
+        page.should have_content("Submit new hours")
+      end
+    end
+
+  end
+end
---
+++ spec/support/hours_entries_helper.rb
@@ -0,0 +1,9 @@
+def fill_in_hours_entries_form(num)
+  fill_in :hours_entry_organization, :with => "Red Cross"
+  fill_in :hours_entry_amount, :with => num
+  fill_in :hours_entry_contact_name, :with => "Supervisor Dude"
+  fill_in :hours_entry_contact_position, :with => "Supervisor"
+  fill_in :hours_entry_contact_phone, :with => "123-456-7890"
+  fill_in :hours_entry_contact_email, :with => "supervisor@dude.com"
+  fill_in :hours_entry_description, :with => Faker::Lorem.paragraph
+end
