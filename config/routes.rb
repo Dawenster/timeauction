6f18139bfcaa7b5ba149c482405f7feb7fe99d2e
David Wen <dstwen@gmail.com>
2014-03-18 22:39:38 -0700

    add press release hidden route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Timeauction::Application.routes.draw do
   get "contact" => "pages#contact", as: :contact
   get "terms-and-conditions" => "pages#terms_and_conditions", as: :terms_and_conditions
   get "donors" => "pages#donors", as: :donors
+  get "press" => "pages#press", as: :press
 
   resources :auctions
   get ":username/auctions" => "auctions#user_auctions", as: :user_auctions
