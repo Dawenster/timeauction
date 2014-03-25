f6c60a30807950142ae0a753fa013edec962fc43
David Wen <dstwen@gmail.com>
2014-03-25 13:47:48 -0700

    comment out validations on auction model: start_date_later_than_today and hours_add_up_to_target

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -7,7 +7,7 @@ class Auction < ActiveRecord::Base
   scope :not_approved, -> { where("approved IS NULL OR approved = false") }
 
   validates :title, :short_description, :description, :about, :target, :start, :end, :volunteer_end_date, :banner, :image, presence: true
-  validate :start_date_later_than_today, :end_date_later_than_start, :volunteer_end_date_later_than_end, :hours_add_up_to_target
+  validate :end_date_later_than_start, :volunteer_end_date_later_than_end#, :hours_add_up_to_target, :start_date_later_than_today
 
   s3_credentials_hash = {
     :access_key_id => ENV['AWS_ACCESS_KEY'],
