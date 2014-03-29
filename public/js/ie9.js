8d604c68a2668a832c3fb20c9b1468be9f365ec9
David Wen <dstwen@gmail.com>
2014-03-28 22:26:19 -0700

    just remove the offcanvas if ie9

--- public/js/ie9.js
+++ public/js/ie9.js
@@ -1,17 +1,3 @@
 (function ( $ ) {
-var width;
-var csToggle = false;
-
-$('.off-canvas-wrap').on('click', '.right-off-canvas-toggle, .exit-off-canvas', function() {
-  width = (csToggle) ? 0 : 250;
-  csToggle = (csToggle) ? false : true;
-
-  $('#calendar-settings').animate({
-    width: width+'px'
-  });
-
-  $('.main-section, .tab-bar').animate({
-    left: '-'+width+'px'
-  });
-});
+  $(".left-off-canvas-menu").remove();
 }( jQuery ));
