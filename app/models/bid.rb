d459f1112481d2f2df77faf96ebfdc1b2d2fdc9b
David Wen <dstwen@gmail.com>
2014-04-03 08:57:15 -0700

    implement successful? and waitlist? methods

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,4 +2,14 @@ class Bid < ActiveRecord::Base
   has_one :hours_entry, :dependent => :destroy
   belongs_to :reward
   belongs_to :user
+
+  def successful?
+    reward = self.reward
+    nth_bid = reward.bids.sort_by{|b|b.created_at}.index(self) + 1
+    nth_bid <= reward.max
+  end
+
+  def waitlist?
+    !self.successful?
+  end
 end
