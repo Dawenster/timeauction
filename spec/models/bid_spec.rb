dc39749475a87089cf4ebb6d5045851aacec0a9b
David Wen <dstwen@gmail.com>
2014-04-03 08:57:05 -0700

    bid method specs for successful? and waitlist?

---
+++ spec/models/bid_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Bid do
+  set(:auction) { FactoryGirl.create :auction_with_rewards, :rewards_count => 2 }
+  set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com" }
+  set(:bid_1) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
+  set(:bid_2) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
+
+  before do
+    auction.rewards.first.update_attributes(:max => 1)
+  end
+
+  context "#successful?" do
+    it "is true" do
+      expect(bid_1.successful?).to eq(true)
+    end
+
+    it "is false" do
+      expect(bid_2.successful?).to eq(false)
+    end
+  end
+
+  context "#waitlist?" do
+    it "is false" do
+      expect(bid_1.waitlist?).to eq(false)
+    end
+
+    it "is true" do
+      expect(bid_2.waitlist?).to eq(true)
+    end
+  end
+end
