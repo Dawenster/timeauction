66268080d2bd948ab18f3eb61db712e90deb8f63
David Wen <dstwen@gmail.com>
2014-04-22 11:10:47 -0700

    make hours entry admin create sort users by display_name

--- app/admin/hours_entry.rb
+++ app/admin/hours_entry.rb
@@ -33,7 +33,7 @@ ActiveAdmin.register HoursEntry do
 
   form do |f|
     f.inputs "Hours Entry" do
-      f.input :user
+      f.input :user, :collection => proc { User.all.sort_by{|u|u.display_name} }
       f.input :bid, :collection => Bid.all.map{ |bid| ["#{bid.user.display_name}: #{bid.reward.title}", bid.id] }.sort
       f.input :amount
       f.input :organization
