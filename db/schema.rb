bd6824e8c86c6f15370e91f11434fdbf05d13b02
David Wen <dstwen@gmail.com>
2014-04-28 15:35:33 -0700

    add name and position to auctions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402202608) do
+ActiveRecord::Schema.define(version: 20140428214245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20140402202608) do
     t.text     "videos"
     t.boolean  "featured",            default: false
     t.integer  "order"
+    t.string   "name"
+    t.string   "position"
   end
 
   create_table "bids", force: true do |t|
