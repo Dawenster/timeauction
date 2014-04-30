734332d4501621dd753cbd14a97941c27874ef04
David Wen <dstwen@gmail.com>
2014-04-30 08:22:04 -0700

    add application and message to bids table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428223818) do
+ActiveRecord::Schema.define(version: 20140430152118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 20140428223818) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "application"
+    t.text     "message"
   end
 
   create_table "hours_entries", force: true do |t|
