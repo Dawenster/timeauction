4728adb3a41363678ae536bc15172b080489279b
David Wen <dstwen@gmail.com>
2014-06-19 17:56:03 -0400

    refactored and added more testimonials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,42 @@ class PagesController < ApplicationController
       "https://s3-us-west-2.amazonaws.com/timeauction/media/news_logos/metro_logo.png",
       "https://s3-us-west-2.amazonaws.com/timeauction/media/news_logos/yahoo_logo.png"
     ]
+    @testimonials = [
+      {
+        :message => "\"Time Auction gave me an excellent opportunity that I couldn't have gotten anywhere else. I really enjoyed it and would do it again! I just wished I had more hours :)\"",
+        :user_avatar => "https://s3-us-west-2.amazonaws.com/timeauction/landing/testimonials/rachel.jpg",
+        :bidder_name => "Rachel Ong",
+        :auction_id => 33
+      },
+      {
+        :message => "\"The meeting went beautifully. Jana was gracious and most helpful. It was a pleasure being part of Time Auction and it is nice to know there are people who care about our world. You make us proud!\"",
+        :user_avatar => "https://s3-us-west-2.amazonaws.com/timeauction/landing/testimonials/helen_sq.jpg",
+        :bidder_name => "H.J. Kovic",
+        :auction_id => 28
+      },
+      {
+        :message => "\"Paul gave insightful advice and offered to help me grow my startup through his connections. Plus he was hilarious and down-to-earth. Awesome work Time Auction!\"",
+        :user_avatar => "https://s3-us-west-2.amazonaws.com/timeauction/landing/testimonials/andy_chou.jpeg",
+        :bidder_name => "Andy Chou",
+        :auction_id => 32
+      },
+      {
+        :message => "\"My chat with Sunjay was invaluable and I've already started using what I learned. I love to volunteer, and through Time Auction I was also able to get even more in return!\"",
+        :user_avatar => "https://s3-us-west-2.amazonaws.com/timeauction/landing/testimonials/susan.jpg",
+        :bidder_name => "Susan Keast",
+        :auction_id => 14
+      },
+      {
+        :message => "\"Thank you Time Auction for your dedication to make this opportunity a reality. I will forever cherish the meeting with Mr. Joyce as a real highlight in my life.\"",
+        :user_avatar => "https://s3-us-west-2.amazonaws.com/timeauction/landing/testimonials/wanda.jpg",
+        :bidder_name => "Wanda Swensen",
+        :auction_id => 15
+      }
+    ].sample(2)
+  end
+
+  def testimonials
+    
   end
 
   def donors
