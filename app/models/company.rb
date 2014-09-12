6ac1b4db2575e5c739c7a11d1049672d86e880c8
David Wen <dstwen@gmail.com>
2014-09-12 16:36:35 -0400

    add validations to companies and email_domains

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < ActiveRecord::Base
   has_many :email_domains
   has_many :users
+
+  validates :url, uniqueness: true
+  validates :url, presence: true
+  validates :name, presence: true
 end
--- app/models/email_domain.rb
+++ app/models/email_domain.rb
@@ -1,3 +1,6 @@
 class EmailDomain < ActiveRecord::Base
   belongs_to :company
+
+  validates :domain, uniqueness: true
+  validates :domain, presence: true
 end
