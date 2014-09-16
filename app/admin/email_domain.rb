355da83b7572c671e12001ac6f67830eb4cc530a
David Wen <dstwen@gmail.com>
2014-09-16 16:05:51 -0700

    add email domain model to activeadmin

---
+++ app/admin/email_domain.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register EmailDomain do
+
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params(
+    :domain,
+    :company_id,
+    :created_at,
+    :updated_at
+  )
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
