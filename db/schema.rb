8b6c71c1c97caa0f7f7a3fca726afcdea1713fb4
David Wen <dstwen@gmail.com>
2014-09-15 21:16:11 -0700

    add company id to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916003539) do
+ActiveRecord::Schema.define(version: 20140916035807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 20140916003539) do
     t.string   "stripe_cus_id"
     t.boolean  "admin",                  default: false, null: false
     t.string   "phone_number"
+    t.integer  "company_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
