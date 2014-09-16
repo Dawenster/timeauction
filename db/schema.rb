7cfefcbbc62c5f74ef078983ef1e1fba7de69d0d
David Wen <dstwen@gmail.com>
2014-09-15 17:36:45 -0700

    create programs migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912194135) do
+ActiveRecord::Schema.define(version: 20140916003539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,13 @@ ActiveRecord::Schema.define(version: 20140912194135) do
     t.boolean  "user_entered",         default: false
     t.string   "dates"
     t.datetime "verification_sent_at"
+  end
+
+  create_table "programs", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "rewards", force: true do |t|
