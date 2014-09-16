d4d199926ec021f1ff30fa7839661abeb59467fa
David Wen <dstwen@gmail.com>
2014-09-15 18:22:01 -0700

    pages for creating / editing program

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,49 @@
+class ProgramsController < ApplicationController
+  def new
+    @program = Program.new
+  end
+
+  def create
+    @program = Program.new(program_params)
+    if @program.save
+      flash[:notice] = "#{@program.name} has been successfully created."
+      redirect_to company_path(@program.company)
+    else
+      flash.now[:alert] = "Please make sure all fields are filled in correctly :)"
+      render "new"
+    end
+  end
+
+  def edit
+    @program = Program.find(params[:id])
+  end
+
+  def update
+    @program = Program.find(params[:id])
+    @program.assign_attributes(program_params)
+    if @program.save
+      flash[:notice] = "#{@program.name} has been successfully updated."
+      redirect_to company_path(@program.company)
+    else
+      flash.now[:alert] = "Please make sure all fields are filled in correctly :)"
+      render "edit"
+    end
+  end
+
+  def destroy
+    program = Program.find(params[:id]).destroy
+    flash[:notice] = "#{program.name} has been deleted."
+    redirect_to root_path
+  end
+
+  private 
+
+  def program_params
+    params.require(:program).permit(
+      :name,
+      :description,
+      :company_id,
+      :_destroy
+    )
+  end
+end
