a5617698d71110628b40b7c3952beeba8318535d
David Wen <dstwen@gmail.com>
2014-09-15 17:25:08 -0700

    can create/edit/delete companies with email domains

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,26 +1,46 @@
 class CompaniesController < ApplicationController
   def show
-    @company = Company.find_by_url(params[:company_url])
+    @company = Company.find_by_url(params[:company_url]) || Company.find(params[:id])
   end
 
   def new
-    
+    @company = Company.new
+    @company.email_domains.build
   end
 
   def create
-    
+    @company = Company.new(company_params)
+    if @company.save
+      flash[:notice] = "#{@company.name} has been successfully submitted."
+      redirect_to company_path(@company)
+    else
+      flash.now[:alert] = "Please make sure all fields are filled in correctly :)"
+      render "new"
+    end
   end
 
   def edit
-    
+    @company = Company.find(params[:id])
+    @company.email_domains.build
   end
 
   def update
-    
+    @company = Company.find(params[:id])
+    cp = remove_blank_email_domains
+    @company.assign_attributes(cp)
+    if @company.save
+      flash[:notice] = "#{@company.name} has been successfully updated."
+      redirect_to company_path(@company)
+    else
+      flash.now[:alert] = "Please make sure all fields are filled in correctly :)"
+      render "edit"
+    end
   end
 
   def destroy
-    
+    company = Company.find(params[:id]).destroy
+    flash[:notice] = "#{company.name} has been deleted."
+    redirect_to root_path
   end
 
   private 
@@ -31,10 +51,25 @@ class CompaniesController < ApplicationController
       :url,
       :logo,
       :background_image,
+      :_destroy,
       email_domains_attributes: [
+        :id,
         :domain,
-        :company_id
+        :company_id,
+        :created_at,
+        :updated_at,
+        :_destroy
       ]
     )
+  end
+
+  def remove_blank_email_domains
+    cp = company_params
+    company_params["email_domains_attributes"].each do |k, v|
+      if v["domain"].blank?
+        cp["email_domains_attributes"] = cp["email_domains_attributes"].except(k)
+      end
+    end
+    return cp
   end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,27 @@
 class Company < ActiveRecord::Base
-  has_many :email_domains
   has_many :users
+  has_many :email_domains, :dependent => :destroy
+  accepts_nested_attributes_for :email_domains, :allow_destroy => true
 
   validates :url, uniqueness: true
-  validates :url, presence: true
-  validates :name, presence: true
+  validates :url, :name, presence: true
+
+  s3_credentials_hash = {
+    :access_key_id => ENV['AWS_ACCESS_KEY'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+
+  has_attached_file :logo, 
+                    :styles => { :thumb => "300x225#", :display => "720x540#" },
+                    :s3_credentials => s3_credentials_hash,
+                    :bucket => ENV['AWS_BUCKET'],
+                    :default_url => "https://s3-us-west-2.amazonaws.com/timeauction/missing-auction-thumb.png"
+
+  has_attached_file :background_image,
+                    :styles => { :thumb => "300x225#", :display => "720x540#" },
+                    :s3_credentials => s3_credentials_hash,
+                    :bucket => ENV['AWS_BUCKET'],
+                    :default_url => "https://s3-us-west-2.amazonaws.com/timeauction/missing-auction-thumb.png"
+
+
 end
