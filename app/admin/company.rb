6f6d8728dfbcb6b76fd9684aba0ec7e6796fe850
David Wen <dstwen@gmail.com>
2014-09-16 15:26:13 -0700

    activeadmin page for company model

---
+++ app/admin/company.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Company do
+
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params(
+    :name,
+    :url,
+    :logo,
+    :background_image,
+    :created_at,
+    :updated_at
+  )
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
