b321b4684e6b7933f972c56545ad0d6768dc08e5
David Wen <dstwen@gmail.com>
2014-09-16 19:07:29 -0700

    test edit features for company pages

--- spec/features/companies/manage_companies_spec.rb
+++ spec/features/companies/manage_companies_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe "manage companies" do
+  set(:user) { FactoryGirl.create :user, :admin => true }
+
+  before do
+    login(user)
+  end
+
   context "create" do
     before do
       visit new_company_path
@@ -21,6 +27,27 @@ describe "manage companies" do
         last_field.set("nike.ca")
         click_on "Create Company"
       end.to change(EmailDomain, :count).by(2)
+    end
+  end
+
+  context "edit", :js => true do
+    set(:company) { FactoryGirl.create :company_with_programs_and_email_domains }
+
+    before do
+      visit edit_company_path(company)
+    end
+
+    it "changes company name" do
+      fill_in :company_name, :with => "Adidas"
+      click_on "Update Company"
+      Company.last.name.should eq("Adidas")
+    end
+
+    it "removes email domain" do
+      expect do
+        all(".remove_nested_fields").first.click
+        click_on "Update Company"
+      end.to change(EmailDomain, :count).by(-1)
     end
   end
 end
