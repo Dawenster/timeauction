c3153410d0ae88f7c26655bcbabe9c144c561c3c
David Wen <dstwen@gmail.com>
2014-11-11 21:18:03 -0800

    add input validation on org select

--- app/assets/javascripts/angular/controllers/organization_selection_controller.js
+++ app/assets/javascripts/angular/controllers/organization_selection_controller.js
@@ -10,4 +10,41 @@ app.controller('OrganizationSelectionCtrl', function($scope) {
   .done(function(result) {
     $scope.organizations = result.organizations;
   });
+
+  $scope.submitOrgs = function() {
+    requiredInputsFilledIn();
+  }
+
+  var requiredInputsFilledIn = function() {
+    hideErrors();
+    var errorCount = 0;
+    var inputs = $(".requiredOrgSelectInput:visible");
+    var firstErrorPosition = 0;
+    
+    for (var i = 0; i < inputs.length; i++) {
+      var value = $(inputs[i]).val().trim();
+      if (value == "") {
+        $(inputs[i]).siblings(".error").toggle();
+        errorCount += 1;
+        if (errorCount == 1) {
+          firstErrorPosition = $(inputs[i]).offset().top - 30;
+        }
+      }
+    };
+
+    if (errorCount == 0) {
+      return true;
+    } else {
+      $('html,body').scrollTop(firstErrorPosition);
+      return false;
+    }
+  }
+
+  var hideErrors = function() {
+    var errors = $(".error:visible");
+
+    for (var i = 0; i < errors.length; i++) {
+      $(errors[i]).toggle();
+    };
+  }
 });
