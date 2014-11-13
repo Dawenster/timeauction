0da7a6f6dbf2e1f474276d5bc24edc0918d2fe67
David Wen <dstwen@gmail.com>
2014-11-13 10:43:20 -0800

    fixed spec bugs: make auction approved and add people descriptor to org helper

--- spec/support/organizations_helper.rb
+++ spec/support/organizations_helper.rb
@@ -1,6 +1,7 @@
 def fill_in_organization_fields
   fill_in :organization_name, :with => "Nike"
   fill_in :organization_url, :with => "nike"
+  fill_in :organization_people_descriptor, :with => "peeps"
   fill_in :organization_email_domains_attributes_0_domain, :with => "nike.com"
 end
 
