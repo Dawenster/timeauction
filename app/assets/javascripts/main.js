4c5b0465441a6f1bc4a415c9724c50a287f6f4e0
David Wen <dstwen@gmail.com>
2014-11-13 09:11:54 -0800

    fix bug where modals don't show up more than once

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,26 +1,4 @@
 $(document).ready(function() {
-  var shownDemo = localStorage.getItem('demo-explanation');
-  // if (!$.cookie('demo-explanation')) {
-  //   $('#demo-explanation').foundation('reveal', 'open', '');
-  // }
-
-  $("body").on("click", ".close-reveal-modal", function(e) {
-    e.preventDefault();
-    $(".reveal-modal").foundation('reveal', 'close', '');
-  });
-
-  $("body").on("click", ".reveal-modal-bg", function(e) {
-    e.preventDefault();
-    $(".reveal-modal").foundation('reveal', 'close', '');
-    // $.cookie('demo-explanation', true);
-  });
-
-  // $("body").on("click", ".demo-close", function(e) {
-  //   e.preventDefault();
-  //   $(".reveal-modal").foundation('reveal', 'close', '');
-  //   $.cookie('demo-explanation', true);
-  // });
-
   // Deep linking for Foundation Tabs
 
   setTimeout(function(){
--- app/assets/javascripts/organizations/organization_modal.js
+++ app/assets/javascripts/organizations/organization_modal.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $('#select-organization-modal').foundation('reveal', 'open', '');
     $.cookie('first_time_sign_in', false)
   }
+
   $(".org-select-dropdown").change(function(e) {
     var selectedText = this.options[e.target.selectedIndex].text;
     if (selectedText == "Other") {
