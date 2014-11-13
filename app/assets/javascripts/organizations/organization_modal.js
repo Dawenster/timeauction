4b7fd20a1aee133efabe11275723e768c5a8c18d
David Wen <dstwen@gmail.com>
2014-11-12 22:53:36 -0800

    show optional field when dropdown "Other" selected

--- app/assets/javascripts/organizations/organization_modal.js
+++ app/assets/javascripts/organizations/organization_modal.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     $('#select-organization-modal').foundation('reveal', 'open', '');
     $.cookie('first_time_sign_in', false)
   }
+  $(".org-select-dropdown").change(function(e) {
+    var selectedText = this.options[e.target.selectedIndex].text;
+    if (selectedText == "Other") {
+      $(this).siblings("input.other-field").toggle();
+    } else if ($("input.other-field").is(":visible")) {
+      $(this).siblings("input.other-field").toggle();
+    }
+  })
 });
