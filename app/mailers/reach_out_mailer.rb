cf0aef695d488d0c6f9c4eef1bae311e4879e0ac
David Wen <dstwen@gmail.com>
2014-11-24 10:26:10 -0800

    add rake task for sending to NA b-schools

--- app/mailers/reach_out_mailer.rb
+++ app/mailers/reach_out_mailer.rb
@@ -14,4 +14,10 @@ class ReachOutMailer < Devise::Mailer
     @first_name = subscriber[:first_name] ? subscriber[:first_name].downcase.titleize.strip : "there"
     mail(from: '"David Wen" <david@timeauction.org>', to: subscriber[:email], subject: "Thank you, from Time Auction")
   end
+
+  def send_to_na_bschool(na_bschool)
+    @first_name = na_bschool[:first_name]
+    @school_in_content = na_bschool[:school_in_content]
+    mail(from: '"David Wen" <david@timeauction.org>', to: na_bschool[:email], subject: "Engaging alumni at #{na_bschool[:school_in_title]}")
+  end
 end
