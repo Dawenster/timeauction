b4a1d2564e33ea9c2696af5b8f362b5421efa832
David Wen <dstwen@gmail.com>
2015-01-27 21:01:56 -0800

    add custom order to landing auctions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,7 +16,7 @@ class PagesController < ApplicationController
         auctions = current_user.current_auctions
       else
         auctions = Auction.not_corporate.approved.current_or_pending
-        auctions = Auction.where(:featured => true) if auctions.empty?
+        auctions = Auction.where(:featured => true).custom_order if auctions.empty?
       end
       @lucky_auction = auctions.sample
       @first_auction = auctions.first
