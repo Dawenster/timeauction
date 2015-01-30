3d3dedd6f78252fdbd8f8c24c4ddf1f0eb2647c8
David Wen <dstwen@gmail.com>
2015-01-30 11:14:08 -0800

    show featured auctions if org auctions empty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,8 +16,8 @@ class PagesController < ApplicationController
         auctions = current_user.current_auctions
       else
         auctions = Auction.not_corporate.approved.current_or_pending
-        auctions = Auction.where(:featured => true).custom_order if auctions.empty?
       end
+      auctions = Auction.where(:featured => true).custom_order if auctions.empty?
       @lucky_auction = auctions.sample
       @first_auction = auctions.first
     end
