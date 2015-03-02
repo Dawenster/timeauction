c5da9c8365fa02f6c2787bb4a786f417b247281c
David Wen <dstwen@gmail.com>
2015-03-02 12:15:02 -0800

    add profiles to active admin

--- app/admin/bid.rb
+++ app/admin/bid.rb
@@ -14,41 +14,39 @@ ActiveAdmin.register Bid do
   #  permitted
   # end
 
-  ActiveAdmin.register Bid do
-    index :as => ActiveAdmin::Views::IndexAsTable do
-      column :id
-      column :user
-      column :reward
-      column :hours
-      column :premium
-      column :created_at
-      column :application
-      column :message
-      column :winning
-      default_actions
-    end
+  index :as => ActiveAdmin::Views::IndexAsTable do
+    column :id
+    column :user
+    column :reward
+    column :hours
+    column :premium
+    column :created_at
+    column :application
+    column :message
+    column :winning
+    default_actions
+  end
 
-    filter :reward, :collection => proc { Reward.all.sort_by{|r|r.title} }
-    filter :user, :collection => proc { User.all.sort_by{|u|u.display_name} }
-    filter :created_at
+  filter :reward, :collection => proc { Reward.all.sort_by{|r|r.title} }
+  filter :user, :collection => proc { User.all.sort_by{|u|u.display_name} }
+  filter :created_at
 
-    csv do
-      column :id
-      column("User ID") { |bid| bid.user.id }
-      column("First name") { |bid| bid.user.first_name }
-      column("Last name") { |bid| bid.user.last_name }
-      column("Display Name") { |bid| bid.user.display_name }
-      column("User Email") { |bid| bid.user.email }
-      column("Reward ID") { |bid| bid.reward.id }
-      column("Reward") { |bid| bid.reward.title }
-      column("Hours") { |bid| bid.hours }
-      column("Premium") { |bid| bid.premium }
-      column("Application") { |bid| bid.application }
-      column("Message") { |bid| bid.message }
-      column("Winning") { |bid| bid.winning }
-      column :created_at
-      # column :successful?
-    end
+  csv do
+    column :id
+    column("User ID") { |bid| bid.user.id }
+    column("First name") { |bid| bid.user.first_name }
+    column("Last name") { |bid| bid.user.last_name }
+    column("Display Name") { |bid| bid.user.display_name }
+    column("User Email") { |bid| bid.user.email }
+    column("Reward ID") { |bid| bid.reward.id }
+    column("Reward") { |bid| bid.reward.title }
+    column("Hours") { |bid| bid.hours }
+    column("Premium") { |bid| bid.premium }
+    column("Application") { |bid| bid.application }
+    column("Message") { |bid| bid.message }
+    column("Winning") { |bid| bid.winning }
+    column :created_at
+    # column :successful?
   end
   
 end
---
+++ app/admin/profile.rb
@@ -0,0 +1,42 @@
+ActiveAdmin.register Profile do
+
+  permit_params :program, :year, :identification_number, :organization_id, :user_id, :department, :location, :data_privacy
+
+  index :as => ActiveAdmin::Views::IndexAsTable do
+    column :id
+    column "User" do |profile|
+      link_to profile.user.display_name, edit_admin_user_path(profile.user)
+    end
+    column "Organization" do |profile|
+      link_to profile.organization.name, edit_admin_organization_path(profile.organization)
+    end
+    column :program
+    column :year
+    column :identification_number
+    column :department
+    column :location
+    column :data_privacy
+    
+    default_actions
+  end
+
+  filter :user, :collection => proc { User.all.sort_by{|u|u.display_name} }
+  filter :organization, :collection => proc { Organization.all.sort_by{|o|o.name} }
+
+  csv do
+    column :id
+    column "User" do |profile|
+      profile.user.display_name
+    end
+    column "Organization" do |profile|
+      profile.organization.name
+    end
+    column :program
+    column :year
+    column :identification_number
+    column :department
+    column :location
+    column :data_privacy
+  end
+
+end
