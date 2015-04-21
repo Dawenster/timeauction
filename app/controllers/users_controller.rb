91a7bf600e5066105080c91815a74c38da36d050
David Wen <dstwen@gmail.com>
2015-04-20 17:18:56 -0700

    add meta for users#show and make public

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController  
-  before_filter :authenticate_user!, :except => [:activity]
+  before_filter :authenticate_user!, :except => [:show]
 
   def show
     @user = User.find(params[:id])
