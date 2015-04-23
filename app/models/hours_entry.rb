76c7a26af47ea15d455e7387262ed2b762ddf584
David Wen <dstwen@gmail.com>
2015-04-23 16:33:33 -0700

    make nonprofit create work in .org bid process too

--- app/models/hours_entry.rb
+++ app/models/hours_entry.rb
@@ -1,11 +1,12 @@
 class HoursEntry < ActiveRecord::Base
   include ApplicationHelper
   
-  validates :amount, :contact_name, :contact_phone, :contact_email, presence: true, :if => :user_entered?
+  validates :organization, :amount, :contact_name, :contact_phone, :contact_email, presence: true, :if => :user_entered?
   validates :contact_email, :format => { :with => /^\s*(([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})[\s\/,;]*)+$/i, :message => "is not a valid email", :multiline => true }, :if => :user_entered?
   validates_numericality_of :amount, greater_than: 0, :if => :user_entered?
 
   before_save :send_verified_email
+  before_save :link_to_nonprofit, :if => :can_link_to_nonprofit?
 
   belongs_to :user
   belongs_to :bid
@@ -52,6 +53,15 @@ class HoursEntry < ActiveRecord::Base
 
   def newly_verified?
     verified && verified_changed? && amount > 0
+  end
+
+  def link_to_nonprofit
+    nonprofit = Nonprofit.find_by_slug_or_create(self.organization)
+    self.nonprofit_id = nonprofit.id
+  end
+
+  def can_link_to_nonprofit?
+    self.organization && amount > 0
   end
 end
 
