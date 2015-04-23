e25dafc8879dc677f60e6681bfbd1d86cdce3c91
David Wen <dstwen@gmail.com>
2015-04-23 13:08:07 -0700

    create slug on nonprofit before save

--- app/models/nonprofit.rb
+++ app/models/nonprofit.rb
@@ -3,4 +3,14 @@ class Nonprofit < ActiveRecord::Base
   has_many :users, :through => :roles
 
   validates :name, presence: true
+
+  before_save :create_slug
+
+  def create_slug
+    self.slug = self.to_slug
+  end
+
+  def to_slug
+    self.name.parameterize
+  end
 end
