ad7c9ab583ed98a69194b4a8467c7a892d0382a3
David Wen <dstwen@gmail.com>
2015-04-27 22:55:00 -0700

    don't show dropdown if no match on autocomplete for nonprofit name

---
+++ app/assets/javascripts/autocomplete_remove_empty.js
@@ -0,0 +1,6 @@
+$("#nonprofit_name").on('autocompleteresponse', function(event, ui) {
+  var content;
+  if (((content = ui.content) != null ? content[0].id.length : void 0) === 0) {
+    $(this).autocomplete('close');
+  }
+});
