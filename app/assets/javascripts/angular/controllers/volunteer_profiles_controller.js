9d7fe52b7dfd66f483f81016873f3dc4e160cdf7
David Wen <dstwen@gmail.com>
2015-04-29 15:16:59 -0700

    initial js structure in place for volunteer profiles

--- app/assets/javascripts/angular/controllers/volunteer_profiles_controller.js
+++ app/assets/javascripts/angular/controllers/volunteer_profiles_controller.js
@@ -1,5 +1,11 @@
 var app = angular.module('timeauction.controllers', []);
 
 app.controller('VolunteerProfilesCtrl', ['$scope', function($scope) {
+  $("body").on("click", ".hours_entry-dates-holder-toggle", function() {
+    $(this).siblings(".hours-entry-dates-holder").toggle()
+  })
   
+  $("body").on("click", ".hours-entry-date-toggle", function() {
+    $(this).siblings(".hours-entry-details").toggle()
+  })
 }]);
