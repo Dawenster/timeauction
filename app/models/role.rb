edc111642fb4cf3e783c4339ee9595d280af22a8
David Wen <dstwen@gmail.com>
2015-04-30 10:35:20 -0700

    only show hours on profile if verified

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,4 +5,8 @@ class Role < ActiveRecord::Base
   def hours
     HoursEntry.earned.where(:user_id => self.user.id, :nonprofit_id => self.nonprofit.id).sum(:amount)
   end
+
+  def can_show_hours?
+    hours > 0
+  end
 end
