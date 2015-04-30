8b209d3d9439ecb0871a302f613658d37d49bdd2
David Wen <dstwen@gmail.com>
2015-04-29 22:31:06 -0700

    add ribbon hours count to profile page

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,8 @@
 class Role < ActiveRecord::Base
   belongs_to :user
   belongs_to :nonprofit
+
+  def hours
+    HoursEntry.earned.where(:user_id => self.user.id, :nonprofit_id => self.nonprofit.id).sum(:amount)
+  end
 end
