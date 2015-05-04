668665d954179656c1d33e880654e35acddf64e9
David Wen <dstwen@gmail.com>
2015-05-04 10:54:53 -0700

    show months on hours_entries#new with ability to add more

--- app/assets/javascripts/angular/controllers/hours_entries_controller.js
+++ app/assets/javascripts/angular/controllers/hours_entries_controller.js
@@ -4,12 +4,21 @@ app.controller('HoursEntryCtrl', ['$scope', "Nonprofits", function($scope, Nonpr
   Nonprofits.syncFields()
 
   $scope.showNewFields = !($(".new-hours-entry-fields").attr("data-has-entries") == "true")
+  $scope.lastMonth = new Date($(".month-selection").last().attr("data-js-date-format"))
 
   updateWithDropdown()
 
   $(".existing-dropdown").change(function() {
     updateWithDropdown()
   });
+
+  $("body").on("click", ".month-selection", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active")
+    } else {
+      $(this).addClass("active")
+    }
+  })
 
   function updateWithDropdown() {
     var ele = $(".existing-dropdown option:selected")
@@ -39,5 +48,20 @@ app.controller('HoursEntryCtrl', ['$scope', "Nonprofits", function($scope, Nonpr
     $("#hours_entry_contact_position").val("")
     $("#hours_entry_contact_phone").val("")
     $("#hours_entry_contact_email").val("")
+  }
+
+  $scope.addMonths = function() {
+    for (var i = 1; i <= 12; i++) {
+      $scope.lastMonth.setMonth($scope.lastMonth.getMonth() - 1)
+      var index = $scope.lastMonth.getMonth()
+      var div = "<div class='small-4 large-3 columns'><div class='month-selection'>" + getMonthName(index) + $scope.lastMonth.getFullYear() + "</div></div>"
+      $(".date-selection-holder-row").append(div)
+      // debugger
+    };
+  }
+
+  function getMonthName(index) {
+    var monthNames = ["Jan. ", "Feb. ", "Mar. ", "Apr. ", "May. ", "Jun. ", "Jul. ", "Aug. ", "Sep. ", "Oct. ", "Nov. ", "Dec. "]
+    return monthNames[index]
   }
 }]);
