fbbc6de4b5e668e58fc127b5c5520a5641681dfb
David Wen <dstwen@gmail.com>
2015-05-04 12:15:26 -0700

    create or find months before save on hours_entries

--- app/models/hours_entry.rb
+++ app/models/hours_entry.rb
@@ -7,6 +7,7 @@ class HoursEntry < ActiveRecord::Base
 
   before_save :send_verified_email
   before_save :link_to_nonprofit, :if => :can_link_to_nonprofit?
+  before_save :link_to_months
 
   has_and_belongs_to_many :months
   belongs_to :user
@@ -68,6 +69,31 @@ class HoursEntry < ActiveRecord::Base
 
   def can_link_to_nonprofit?
     self.organization && amount > 0
+  end
+
+  def link_to_months
+    dates.split(", ").each do |month|
+      split_month = month.split(". ")
+      month_name = split_month.first
+      month_year = split_month.last.to_i
+
+      if valid_month?(month_name) && month_year > 0
+        month = Month.find_by_name(month_name)
+        if month.nil?
+          month = Month.create(
+            :name => month_name,
+            :year => month_year,
+            :as_date => DateTime.strptime("#{month_year}-#{month_name}", "%Y-%b")
+          )
+        end
+        self.months << month
+      end
+    end
+  end
+
+  def valid_month?(month)
+    valid_months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    return valid_months.include?(month)
   end
 end
 
