6b4a46a62b17d96768cab31634cb9ef767d8e18c
David Wen <dstwen@gmail.com>
2015-05-13 09:30:45 -0700

    add webinar and draw to rewards

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512203054) do
+ActiveRecord::Schema.define(version: 20150513163007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -184,6 +184,8 @@ ActiveRecord::Schema.define(version: 20150512203054) do
     t.datetime "updated_at"
     t.boolean  "limit_bidders"
     t.boolean  "premium",       default: false
+    t.boolean  "webinar"
+    t.boolean  "draw"
   end
 
   create_table "roles", force: true do |t|
