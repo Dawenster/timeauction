551cfa36e56e99c901f09e08bee8dba2dbe501c8
David Wen <dstwen@gmail.com>
2015-05-18 19:01:10 -0700

    initial specs for user profile page

--- spec/features/users/user_activity_spec.rb
+++ spec/features/users/user_activity_spec.rb
@@ -1,105 +1,50 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe "user profile page" do
-  # subject { page }
+describe "user profile page" do
+  subject { page }
 
-  # set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com", :admin => true }
-  # set(:auction) { FactoryGirl.create :auction_with_rewards, :rewards_count => 2, :user => user }
-  # set(:bid_1) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
+  set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com", :admin => true }
+  set(:auction) { FactoryGirl.create :auction_with_rewards, :rewards_count => 2, :user => user }
+  set(:bid_1) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
 
-  # before do
-  #   login(user)
-  # end
+  before do
+    login(user)
+  end
 
-  # context "bid on" do
-  #   it "can see auctions bid on", :js => true do
-  #     visit user_path(user)
-  #     page.should have_content(auction.title, visible: true)
-  #   end
+  context "new user" do
+    it "sees prompt to log hours", :js => true do
+      visit user_path(user)
+      page.should have_content("Let's start building your profile", visible: true)
+    end
+  end
 
-  #   it "cannot see auctions bid on in other tabs", :js => true do
-  #     auction.update_attributes(:user_id => 200)
-  #     visit user_path(user)
-  #     click_on "Saved"
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Submitted"
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Approved"
-  #     page.should_not have_content(auction.title, visible: true)
-  #   end
-  # end
+  context "active user" do
+    it "can see auctions bid on", :js => true do
+      HoursEntry.create(:amount => 10, :user_id => user.id, :organization => "Red Cross")
+      visit user_path(user)
+      page.should have_content(auction.title, visible: true)
+    end
 
-  # context "saved" do
-  #   it "can see saved auctions", :js => true do
-  #     visit user_path(user)
-  #     click_on "Saved"
-  #     page.should have_content(auction.title, visible: true)
-  #   end
+    context "edit about" do
+      before do
+        visit user_path(user)
+        find(".edit-about-me").click
+      end
 
-  #   it "cannot see saved auctions in other tabs", :js => true do
-  #     bid_1.update_attributes(:user_id => 200)
-  #     visit user_path(user)
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Submitted"
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Approved"
-  #     page.should_not have_content(auction.title, visible: true)
-  #   end
-  # end
+      it "toggles textarea", :js => true do
+        page.should have_css(".about-me-input", visible: true)
+      end
 
-  # context "submitted" do
-  #   it "can see submitted auctions", :js => true do
-  #     auction.update_attributes(:submitted => true)
-  #     visit user_path(user)
-  #     click_on "Submitted"
-  #     page.should have_content(auction.title, visible: true)
-  #   end
-
-  #   it "cannot see submitted auctions in other tabs", :js => true do
-  #     bid_1.update_attributes(:user_id => 200)
-  #     auction.update_attributes(:submitted => true)
-  #     visit user_path(user)
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Saved"
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Approved"
-  #     page.should_not have_content(auction.title, visible: true)
-  #   end
-  # end
-
-  # context "approved" do
-  #   it "can see approved auctions", :js => true do
-  #     auction.update_attributes(:submitted => true, :approved => true)
-  #     visit user_path(user)
-  #     click_on "Approved"
-  #     page.should have_content(auction.title, visible: true)
-  #   end
-
-  #   it "cannot see approved auctions in other tabs", :js => true do
-  #     bid_1.update_attributes(:user_id => 200)
-  #     auction.update_attributes(:submitted => true, :approved => true)
-  #     visit user_path(user)
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Saved"
-  #     page.should_not have_content(auction.title, visible: true)
-  #     click_on "Submitted"
-  #     page.should_not have_content(auction.title, visible: true)
-  #   end
-  # end
-
-  # context "pending approval tag" do
-    # it "shows when pending_approval" do
-    #   auction.update_attributes(:submitted => true)
-    #   visit user_path(user)
-    #   click_on "Saved"
-    #   page.should have_css(".auction-not-yet-approved")
-    # end
-
-    # it "does not show when approved" do
-    #   auction.update_attributes(:submitted => true, :approved => true)
-    #   visit user_path(user)
-    #   click_on "Approved"
-    #   page.should_not have_css(".auction-not-yet-approved")
-    # end
-  # end
-# end
+      it "saves data", :js => true do
+        about_text = "I'm so good at this and that."
+        find(".about-me-input").set(about_text)
+        within ".about-me-input-holder" do
+          click_on "Save"
+        end
+        page.should have_content(about_text, visible: true)
+        user.reload
+        expect(user.about).to eq(about_text)
+      end
+    end
+  end
+end
