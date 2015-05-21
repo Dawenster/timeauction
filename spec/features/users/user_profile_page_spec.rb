c4a8c93f2d504ae47ef65bf78ba5877021bffc10
David Wen <dstwen@gmail.com>
2015-05-20 21:21:37 -0700

    add role title and description specs

--- spec/features/users/user_profile_page_spec.rb
+++ spec/features/users/user_profile_page_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe "user profile page" do
   subject { page }
 
-  set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com", :admin => true }
+  set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com" }
   set(:auction) { FactoryGirl.create :auction_with_rewards, :rewards_count => 2, :user => user }
   set(:bid_1) { FactoryGirl.create :bid, :reward_id => auction.rewards.first.id, :user_id => user.id }
+  # set(:entry) { FactoryGirl.create :hours_entry, :amount => 10, :bid_id => bid_1.id, :user_id => user.id }
 
   before do
     login(user)
@@ -44,6 +45,39 @@ describe "user profile page" do
         page.should have_content(about_text, visible: true)
         user.reload
         expect(user.about).to eq(about_text)
+      end
+    end
+
+    context "edit roles" do
+      before do
+        HoursEntry.create(:amount => 10, :user_id => user.id, :organization => "Red Cross")
+        visit user_path(user)
+        find(".edit-role-title-text").click
+      end
+
+      it "toggles edit for position title and description", :js => true do
+        page.should have_css(".edit-role-input", visible: true)
+        page.should have_css(".role-description-input", visible: true)
+      end
+
+      it "saves role title", :js => true do
+        role_title = "The supreme leader"
+        find(".edit-role-input").set(role_title)
+        within ".role-description" do
+          click_on "Save"
+        end
+        page.should have_content(role_title, visible: true)
+        expect(Role.last.title).to eq(role_title)
+      end
+
+      it "saves role description", :js => true do
+        role_description = "I do a great job no matter what"
+        find(".role-description-input").set(role_description)
+        within ".role-description" do
+          click_on "Save"
+        end
+        page.should have_content(role_description, visible: true)
+        expect(Role.last.description).to eq(role_description)
       end
     end
   end
