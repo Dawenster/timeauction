65325dfa46029364d8bae4a4623d23f4c406e815
David Wen <dstwen@gmail.com>
2015-05-31 11:13:51 -0700

    put first time sign in modal back in

--- app/assets/javascripts/organizations/organization_modal.js
+++ app/assets/javascripts/organizations/organization_modal.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   if ($.cookie('first_time_sign_in') == "true") {
-    // $('#select-organization-modal').foundation('reveal', 'open', '');
+    $('#select-organization-modal').foundation('reveal', 'open', '');
     $.cookie('first_time_sign_in', false)
   }
 });
