c148ed5effe8a1dc8dfa551fb85c1514ef226039
David Wen <dstwen@gmail.com>
2015-06-11 19:48:05 -0700

    add user_id to donations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611203031) do
+ActiveRecord::Schema.define(version: 20150612024426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20150611203031) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "stripe_charge_id"
+    t.integer  "user_id"
   end
 
   create_table "email_domains", force: true do |t|
