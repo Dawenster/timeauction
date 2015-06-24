bc9e2ea2c9b3658351378607555739810105c1ff
David Wen <dstwen@gmail.com>
2015-06-23 21:22:39 -0700

    add points to hours entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612024426) do
+ActiveRecord::Schema.define(version: 20150624042159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20150612024426) do
     t.integer  "nonprofit_id"
     t.integer  "month"
     t.integer  "year"
+    t.integer  "points"
   end
 
   create_table "nonprofits", force: true do |t|
