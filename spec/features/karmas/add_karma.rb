0441ad6a8e6bd32a6b8ff0e5d8121044195afcf8
David Wen <dstwen@gmail.com>
2015-06-25 21:57:19 -0700

    write pending tests for adding karma - general and donations contexts

---
+++ spec/features/karmas/add_karma.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe "add karma", :js => true do
+  subject { page }
+
+  set(:user) { FactoryGirl.create :user, :email => "johndoe2@email.com", :admin => true }
+
+  context "general" do
+    it "cannot add if nothing selected"
+
+    it "cannot add if nothing selected"
+
+    it "shows correct current karma"
+  end
+
+  context "donations" do
+    it "shows karma points as soon as section opened"
+
+    it "karma points gone when section closed"
+
+    it "$10 pre-selected"
+
+    it "can change to other pre-selected amounts"
+
+    it "retains selection ratio after selecting other amounts"
+
+    context "custom amount" do
+      it "can select and input"
+
+      context "errors" do
+        it "shows when letter"
+
+        it "shows when negative"
+
+        it "shows when too large"
+      end
+    end
+
+    context "sliders" do
+      it "donations changes value"
+
+      it "tip changes value"
+
+      it "donations changes tip value correctly"
+
+      it "tip changes donation value correctly"
+    end
+
+    context "no existing card" do
+      it "doesn't show Use Your Card"
+
+      it "prompts Stripe checkout when click 'Add'"
+    end
+
+    context "existing card" do
+      it "shows checkbox already selected as default"
+
+      it "shows correct card details"
+
+      context "checkbox selected" do
+        it "shows warning under 'Add'"
+      end
+
+      context "checkbox unselected" do
+        it "removes warning"
+
+        it "prompts Stripe checkout"
+      end
+    end
+  end
+end
