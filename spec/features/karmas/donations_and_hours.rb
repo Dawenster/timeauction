15d7ff21d8832c81259fa58380ed4ed793bccf05
David Wen <dstwen@gmail.com>
2015-06-29 13:39:03 -0700

    rename spec files under karmas

---
+++ spec/features/karmas/donations_and_hours.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "add karma donations and hours", :js => true do
+  subject { page }
+
+  set(:user) { FactoryGirl.create :user, :email => "johndoe@email.com", :admin => true }
+  set(:nonprofit) { FactoryGirl.create :nonprofit }
+
+  
+end
