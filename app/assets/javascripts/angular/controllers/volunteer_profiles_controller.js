9c7f57d6397a44ac2d36154c003128ac8552de80
David Wen <dstwen@gmail.com>
2015-07-01 16:50:59 -0700

    show details toggles hours details

--- app/assets/javascripts/angular/controllers/volunteer_profiles_controller.js
+++ app/assets/javascripts/angular/controllers/volunteer_profiles_controller.js
@@ -1,11 +1,6 @@
 var app = angular.module('timeauction');
 
 app.controller('VolunteerProfilesCtrl', ['$scope', 'Roles', 'Users', function($scope, Roles, Users) {
-  $("body").on("click", ".hours_entry-dates-holder-toggle", function() {
-    toggleDateToggles($(this))
-    toggleDatesHolder($(this))
-  })
-
   $("body").on("click", ".hours-entry-date-toggle", function() {
     $(this).siblings(".hours-entry-details").toggle()
   })
@@ -33,16 +28,6 @@ app.controller('VolunteerProfilesCtrl', ['$scope', 'Roles', 'Users', function($s
     $(this).toggle()
   })
 
-  function toggleDateToggles(ele) {
-    ele.toggle()
-    ele.siblings(".plus-toggle").toggle()
-    ele.siblings(".minus-toggle").toggle()
-  }
-
-  function toggleDatesHolder(ele) {
-    ele.siblings(".hours-entry-dates-holder").toggle()
-  }
-
   function toggleBackFromBottomSection(ele) {
     ele.parents(".role-description").siblings(".top-section").find(".edit-role-title-input-holder").toggle()
     ele.siblings(".role-description-input-holder").toggle()
@@ -59,5 +44,19 @@ app.controller('VolunteerProfilesCtrl', ['$scope', 'Roles', 'Users', function($s
 
     ele.parents(".role-description").siblings(".top-section").find(".edit-role-title").html(title)
     ele.siblings(".edit-role-description-text").find(".edit-role-description").html(Users.makeIntoParagraphs(description))
+  }
+
+  $scope.showVolunteerDetails = function(event) {
+    var ele = null
+    if ($(event.target).hasClass("show-details-button")) {
+      ele = $(event.target).siblings(".hours-entry-dates-holder")
+      $(event.target).find(".show-details-text").toggle()
+      $(event.target).find(".hide-details-text").toggle()
+    } else {
+      ele = $(event.target).parents(".show-details-button").siblings(".hours-entry-dates-holder")
+      $(event.target).toggle()
+      $(event.target).siblings(".other-detail-type").toggle()
+    } 
+    ele.toggle()
   }
 }]);
