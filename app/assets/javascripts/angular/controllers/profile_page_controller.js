34f7b592bea4f5abc394094966c0e647dc720f7d
David Wen <dstwen@gmail.com>
2015-07-01 12:18:13 -0700

    created profile page tabs

--- app/assets/javascripts/angular/controllers/profile_page_controller.js
+++ app/assets/javascripts/angular/controllers/profile_page_controller.js
@@ -2,6 +2,7 @@ var app = angular.module('timeauction');
 
 app.controller('ProfilePageCtrl', ['$scope', 'Users', function($scope, Users) {
   $scope.showAboutInput = false
+  $scope.volunteerSection = true
 
   $scope.toggleAboutInput = function() {
     $(".about-me-input-holder").toggle()
@@ -24,5 +25,25 @@ app.controller('ProfilePageCtrl', ['$scope', 'Users', function($scope, Users) {
       text = $(".about-me-input-holder").attr("data-empty-text")
     }
     $(".about-me-text").html(Users.makeIntoParagraphs(text))
+  }
+
+  $scope.tabSelection = function(event) {
+    var section = $(event.target).text()
+    if (section == "Volunteering") {
+      allSectionsFalse()
+      $scope.volunteerSection = true
+    } else if (section == "Donations") {
+      allSectionsFalse()
+      $scope.donationSection = true
+    } else { // Bids
+      allSectionsFalse()
+      $scope.bidsSection = true
+    }
+  }
+
+  function allSectionsFalse() {
+    $scope.volunteerSection = false
+    $scope.donationSection = false
+    $scope.bidsSection = false
   }
 }]);
