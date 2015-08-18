aa434db05017b42ddd9faa4df198c8376012d43f
David Wen <dstwen@gmail.com>
2015-08-17 22:15:42 -0700

    Remove alert box before reloading on landing page email subscribe

--- app/assets/javascripts/subscribers/subscribe.js
+++ app/assets/javascripts/subscribers/subscribe.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("body").on("ajax:success", ".new_subscriber", function(e, data) {
+    $(".alert-box").remove() // In case there is already an alert box showing
     location.reload(false)
     $('html,body').scrollTop(0);
     // $(".subscriber-callback-message").html(data.message);
