8a94c86d17e4781875c9166aa4930bc9dd810862
David Wen <dstwen@gmail.com>
2015-10-25 11:24:39 -0700

    Fix Stripe error when no existing card

--- app/assets/javascripts/angular/controllers/account_settings_controller.js
+++ app/assets/javascripts/angular/controllers/account_settings_controller.js
@@ -7,24 +7,26 @@ app.controller('AccountSettingsCtrl', ['$scope', function($scope) {
   var stripePublishableKey = $(".update-card-holder").attr("data-stripe-publishable-key")
   var url = $(".update-card-holder").attr("data-donate-path")
 
-  var handler = StripeCheckout.configure({
-    key: stripePublishableKey,
-    image: "https://s3-us-west-2.amazonaws.com/timeauction/ta-dark-logo.png",
-    token: function(token) {
-      // Use the token to create the charge with a server-side script.
-      // You can access the token ID with `token.id`
-      $.ajax({
-        url: $(".update-card-holder").attr("data-update-url"),
-        method: "post",
-        data: {
-          token: token,
-          cardId: $scope.cardId
-        }
-      }).done(function(data) {
-        location.reload(false)
-      })
-    }
-  });
+  if ($(".update-card-holder").length > 0) {
+    var handler = StripeCheckout.configure({
+      key: stripePublishableKey,
+      image: "https://s3-us-west-2.amazonaws.com/timeauction/ta-dark-logo.png",
+      token: function(token) {
+        // Use the token to create the charge with a server-side script.
+        // You can access the token ID with `token.id`
+        $.ajax({
+          url: $(".update-card-holder").attr("data-update-url"),
+          method: "post",
+          data: {
+            token: token,
+            cardId: $scope.cardId
+          }
+        }).done(function(data) {
+          location.reload(false)
+        })
+      }
+    });
+  }
 
   $scope.updateCard = function($event) {
     var email = $(".update-card-holder").attr("data-user-email")
