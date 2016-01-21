9d602f32362d476e6eb9ff2b41336659a1158308
David Wen <dstwen@gmail.com>
2016-01-20 19:27:18 -0800

    Auto fill in fields when hiding

--- app/assets/javascripts/angular/controllers/add_hours_controller.js
+++ app/assets/javascripts/angular/controllers/add_hours_controller.js
@@ -3,5 +3,27 @@ var app = angular.module('timeauction');
 app.controller('AddHoursCtrl', ['$scope', function($scope) {
   $("body").on("change", "#not-decided", function() {
     $(".not-decided").toggle()
+    if ($(".not-decided:visible").length > 0) {
+      clearFieldsForNotDecided()
+    } else {
+      fillInFieldsForNotDecided()
+    }
   })
+
+  function fillInFieldsForNotDecided() {
+    $(".nonprofit-name-autocomplete").val("TBD")
+    var minHours = $("#not-decided").data("min-hours")
+    $(".hours").val(minHours)
+    $(".user_hours_entries_contact_name input").val("TBD")
+    $(".user_hours_entries_contact_position input").val("TBD")
+    $(".user_hours_entries_contact_email input").val("tbd@tbd.com")
+  }
+
+  function clearFieldsForNotDecided() {
+    $(".nonprofit-name-autocomplete").val("")
+    $(".hours").val("")
+    $(".user_hours_entries_contact_name input").val("")
+    $(".user_hours_entries_contact_position input").val("")
+    $(".user_hours_entries_contact_email input").val("")
+  }
 }]);
