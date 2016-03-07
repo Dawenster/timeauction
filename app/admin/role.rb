00307ee6561c0873c358a872574f1ee438ebaab6
David Wen <dstwen@gmail.com>
2016-03-06 22:06:40 -0800

    Update roles admin page

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -5,4 +5,32 @@ ActiveAdmin.register Role do
     :created_at,
     :updated_at
   )
+
+  index :as => ActiveAdmin::Views::IndexAsTable do
+    column :id
+    column :user
+    column :nonprofit
+    column :title
+    column :description
+    actions
+  end
+
+
+  csv do
+    column :id
+    column "User ID" do |role|
+      role.user.id
+    end
+    column "User" do |role|
+      role.user.display_name
+    end
+    column "Nonprofit ID" do |role|
+      role.nonprofit.id
+    end
+    column "Nonprofit" do |role|
+      role.nonprofit.name
+    end
+    column :title
+    column :description
+  end
 end
